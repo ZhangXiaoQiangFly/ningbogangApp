adc68fc2a7dc5a01d2c70ecf33249775fa71a07f
Administrator <YJSong411411>
2021-01-20 17:48:19 +0800

    界面优化

--- src/app/setting/setting.page.ts
+++ src/app/setting/setting.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { AlertController, NavController } from "@ionic/angular";
 
 @Component({
   selector: "app-setting",
@@ -7,11 +7,43 @@ import { NavController } from '@ionic/angular';
   styleUrls: ["./setting.page.scss"],
 })
 export class SettingPage implements OnInit {
-  constructor(private nav: NavController) {}
+  constructor(
+    private nav: NavController,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {}
 
   back() {
-    this.nav.back(); 
+    this.nav.back();
+  }
+  exit() {
+    this.AlertConfirm();
+  }
+  // 退出提示
+  async AlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: "my-custom-class",
+      header: "",
+      message: "退出当前用户？",
+      buttons: [
+        {
+          text: "取消",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: blah => {
+            console.log("Confirm Cancel: blah");
+          },
+        },
+        {
+          text: "确定",
+          handler: () => {
+            this.nav.navigateRoot("/login");
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
